07a8ca6dd7b5301a2bb0e5a413d09e48d3acede3
hujji <kstucoder@gmail.com>
2026-05-07 20:20:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ def main_kb() -> ReplyKeyboardMarkup:
 
 def secret_kb() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text="🚪 Leave Chat")]],
+        keyboard=[[KeyboardButton(text="🚪 Leave playlist")]],
         resize_keyboard=True,
         input_field_placeholder="Type your message..."
     )
